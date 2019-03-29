7f6cfe93331590e1b4ffe9218cf5bc02d0cf3963
nileshsingh067 <nileshsingh067@gmail.com>
2019-03-29 15:32:42 +0530

    first commit

---
+++ src/test/java/com/print/and/post/PrintAndPostConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.print.and.post;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrintAndPostConfigServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
